1d3e07a2f46c822540f7422edca1b0858b1e23c1
Marko <mbudisic@clarkson.edu>
2023-05-02 14:09:14 -0400

    Plotting the velocity field and speed

--- src/evaluate_velocity_field.jl
+++ src/evaluate_velocity_field.jl
@@ -6,12 +6,10 @@ idx = 200
 px = LinRange(-.2,.2,31)
 py = LinRange(-.2,.2,31)
 
-p = [ ComponentVector(x=x, y=y) for x in u[1], y in u[2]]
-p = ComponentArray( 
-    x= vec( map( pp -> pp.x, p) ),  
-    y= vec( map( pp -> pp.y, p) ) )
+D = vortex_biot_savart( (px, py), γ, 0, sol.u[idx]; core=0.005 )
 
+shapeme(v) = reshape( v, (length(px), length(py)) )
 
-D2 = vortex_biot_savart( (px, py), γ, 0, sol.u[idx]; core=0.005 )
-
-arrows(px, py', D.x .* Δ./100, D.y .* Δ./100)
+ax = Makie.heatmap(px, py, shapeme( hypot.( D.x, D.y ) ) )
+Makie.arrows!(ax.axis,px, py, shapeme(D.x * Δ), shapeme(D.y * Δ);transparency=true)
+ax
